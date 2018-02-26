30269246620e46cac7ef7eebe5ff131e3b140ca5
JhonnyBravo <sanfranceshika5@gmail.com>
2018-02-26 21:56:02 +0900

    deleted directory: sql
    deleted: sql/{
      deleteAddressMaster.sql, deleteUpdateData.sql,
      insertAddressMaster.sql, selectAddData.sql,
      selectDelData.sql
    }

--- sql/deleteAddressMaster.sql
+++
@@ -1,9 +0,0 @@
-DELETE *
-FROM tblAddressMaster
-WHERE 郵便番号 IN
-    (SELECT 郵便番号
-     FROM selectDelData)
-  AND 住所 IN
-    (SELECT 住所
-     FROM selectDelData);
-
--- sql/insertAddressMaster.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tblAddressMaster
-SELECT *
-FROM selectAddData;
-
